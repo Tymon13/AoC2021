d5a4b83a9331b544501fb0724509e78e5fd2bd42
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-10-08 18:54:15 +0200

    Day 1: part one solution

--- day1/main.cpp
+++ day1/main.cpp
@@ -2,20 +2,34 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <iterator>
 
 std::vector<int> getInput(const std::string& filename)
 {
     std::vector<int> result;
     std::ifstream stream{filename};
-    while(stream >> result.emplace_back())
-        ;
+    while (stream >> result.emplace_back())
+    {
+    }
     result.pop_back(); // last insertion failed
+    return result;
+}
+
+int countIncreasing(const std::vector<int>& input)
+{
+    int result{};
+    for (auto it = input.begin(); it != std::prev(input.end()); ++it)
+    {
+        if (*it < *std::next(it))
+        {
+            ++result;
+        }
+    }
     return result;
 }
 
 int main()
 {
     auto input = getInput("../day1/input");
-    for(int i: input)
-        std::cout << i << "\n";
+    std::cout << "Number of increasing values: " << countIncreasing(input);
 }
