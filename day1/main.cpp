843135249faee7ed407096a3958bde4530888b16
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-10-08 19:04:20 +0200

    Day 1: part two solution

--- day1/main.cpp
+++ day1/main.cpp
@@ -28,8 +28,26 @@ int countIncreasing(const std::vector<int>& input)
     return result;
 }
 
+int countIncreasingInSlidingWindow(const std::vector<int>& input)
+{
+    int result{};
+    for (auto it = input.begin(); it != std::prev(input.end(), 3); ++it)
+    {
+        // naive implementation, doesn't matter for this size of input
+        int sum1 = *it + *(it + 1) + *(it + 2);
+        int sum2 = *(it + 1) + *(it + 2) + *(it + 3);
+        if (sum2 > sum1)
+        {
+            ++result;
+        }
+    }
+    return result;
+}
+
 int main()
 {
     auto input = getInput("../day1/input");
-    std::cout << "Number of increasing values: " << countIncreasing(input);
+    std::cout << "Number of increasing values: " << countIncreasing(input) << '\n';
+    std::cout << "Number of increasing values in three-measurement sliding window: "
+              << countIncreasingInSlidingWindow(input);
 }
